885e3dcc9fd79c9442d80b1fdf4df909aa6e7af0
ffdd270 <ffdd270@gmail.com>
2021-01-04 14:01:35 +0900

    Fix [WIP] : Make Tokens.

--- cpp_function_finder/finder.py
+++ cpp_function_finder/finder.py
@@ -1,4 +1,4 @@
-test_cpp_functions : list[str] = [
+test_cpp_functions: list = [
     'void test();',
     'int tato();', 
     'int * ptr_return_type();',
@@ -6,27 +6,28 @@ test_cpp_functions : list[str] = [
     'const int test_const_();',
     # 여기부터는 파라미터 테스트
     'void test_params( int a, int b );',
-    'int test_param2s( int & a, int * b );'
+    'int test_param2s( int & a, int * b );',
+    'void ImGui::Temp(int a, int b);'
 ]
 
 
-def make_token( function_str : str ): 
-    tokens: list[str] = []
-    token: str = ""
+def make_token(function_str: str):
+    tokens: dict = {"return_type": "", "name": "", "namespace": "", "params": []}
+    token: str  = ""
     space_flag: bool = False
     param_flag: bool = False
 
-    for char in function_str: 
+    for char in function_str:
+        params: list = tokens["params"];
+
         if char == ";":  # 토큰 끝. 이건 확실하고..
-            if token != "": 
-                tokens.append( token )
+            if token != "":  # 끝 부분에 있는 건 리턴 파라미터라도 가정하자.
+                params.append(token)
             token = ""
             continue
-
         if char == " ":  # 이건 아닐 수도 있음. space 플래그를 올리자.
             space_flag = True
             continue
-
         if space_flag and ( char == "*" or char == "&" ):  # 포인터, 레퍼런스면 토큰 확장.  
             space_flag = False
             token += char
@@ -39,7 +40,7 @@ def make_token( function_str : str ):
         elif space_flag:  # 스페이스 플래그가 올라간 상태에서 새로운 문장은 새로운 토큰.
             space_flag = False
             if token != "": 
-                tokens.append( token )
+                tokens.append(token)
             token = char
         else:  # 아니라면 기존 토큰을 더 길게.
             token += char
@@ -47,5 +48,13 @@ def make_token( function_str : str ):
     return tokens
 
 
-for function_str in test_cpp_functions: 
-    print(make_token(function_str))
+def make_function(tokens: list):
+    bind_str : str = '''bindFunc("{0}", &{0})'''.format(tokens[1]);
+    return bind_str
+
+
+for function_str in test_cpp_functions:
+    token = make_token(function_str)
+    print(token)
+    print(make_function(token))
+
