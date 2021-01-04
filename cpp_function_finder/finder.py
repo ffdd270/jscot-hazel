fd88e9dcbee49ba724dd75d0372d89879f879482
ffdd270 <ffdd270@gmail.com>
2021-01-04 13:03:27 +0900

    Add : .gitignore

--- cpp_function_finder/finder.py
+++ cpp_function_finder/finder.py
@@ -11,11 +11,10 @@ test_cpp_functions : list[str] = [
 
 
 def make_token( function_str : str ): 
-    tokens : list[str] = []
-    token : str = ""
-    space_flag : bool = False
-    param_flag : bool = False
-    
+    tokens: list[str] = []
+    token: str = ""
+    space_flag: bool = False
+    param_flag: bool = False
 
     for char in function_str: 
         if char == ";":  # 토큰 끝. 이건 확실하고..
@@ -24,29 +23,29 @@ def make_token( function_str : str ):
             token = ""
             continue
 
-        if char == " ": # 이건 아닐 수도 있음. space 플래그를 올리자.
+        if char == " ":  # 이건 아닐 수도 있음. space 플래그를 올리자.
             space_flag = True
             continue
 
-        if space_flag and ( char == "*" or char == "&" ):  # 포인터, 레퍼런스면 토큰 확장. 
+        if space_flag and ( char == "*" or char == "&" ):  # 포인터, 레퍼런스면 토큰 확장.  
             space_flag = False
             token += char
-        elif char == "(": # 새로운 토큰 시작
+        elif char == "(":  # 새로운 토큰 시작
             param_flag = True
-        elif char == ")": # 파람 플래그 끝
+        elif char == ")":  # 파람 플래그 끝
             param_flag = False
         elif char == ",": 
             continue # SKIP.
-        elif space_flag: # 스페이스 플래그가 올라간 상태에서 새로운 문장은 새로운 토큰. 
+        elif space_flag:  # 스페이스 플래그가 올라간 상태에서 새로운 문장은 새로운 토큰.
             space_flag = False
             if token != "": 
                 tokens.append( token )
             token = char
-        else: # 아니라면 기존 토큰을 더 길게.
+        else:  # 아니라면 기존 토큰을 더 길게.
             token += char
 
     return tokens
 
 
 for function_str in test_cpp_functions: 
-    print( make_token( function_str ) )
+    print(make_token(function_str))
