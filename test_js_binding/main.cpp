08e13253708f57e00c70807c3a6723d552715f2b
ffdd270 <ffdd270@gmail.com>
2021-01-08 01:29:01 +0900

    Impl : 바인딩

--- test_js_binding/main.cpp
+++ test_js_binding/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <duktape.h>
+#include <functional>
 
 static duk_ret_t native_print(duk_context *ctx) {
 	duk_push_string(ctx, " ");
@@ -22,10 +23,73 @@ static duk_ret_t native_adder(duk_context *ctx) {
 	return 1;  /* one return value */
 }
 
-void native_bug()
+int hello2( int b )
+{
+	return b;
+}
+
+std::unordered_map<uint32_t, void *> magic_tbl;
+
+template <typename Type>
+Type getParam(duk_context * ctx, int place)
+{
+	return Type();
+}
+
+template <>
+int getParam(duk_context * ctx, int place)
+{
+	return duk_get_int( ctx, place );
+}
+
+template <typename Type>
+void getReturn( duk_context * ctx, Type & value )
 {
 
 }
+template <>
+void getReturn( duk_context * ctx, int & value )
+{
+	duk_push_int( ctx, value );
+}
+
+
+template <typename ReturnType, typename Param1>
+duk_ret_t caller(duk_context * ctx, ReturnType(* func)(Param1) )
+{
+	ReturnType rtn = func( getParam<Param1>(ctx, 0) );
+	getReturn( ctx, rtn );
+	return 1;
+}
+
+template <typename ReturnType, typename Param1, typename Param2>
+duk_ret_t caller(duk_context * ctx, ReturnType(* func)(Param1, Param2) )
+{
+	ReturnType rtn = func( getParam<Param1>(ctx, 0) );
+	getReturn( ctx, rtn );
+	return 1;
+}
+
+
+template <typename ReturnType, typename ...Params>
+duk_ret_t realBindFunc(duk_context * ctx)
+{
+	duk_push_current_function( ctx );
+	duk_get_prop_string( ctx, -1, "_func_ptr" );
+	auto func = reinterpret_cast<ReturnType (*)(Params...)>( duk_to_pointer( ctx,  -1  ) );
+	caller( ctx, func );
+	return 1;
+}
+
+template<typename ReturnType, typename ...Params >
+void bindFunc(  duk_context * ctx, const char * name, ReturnType (* func_ptr)(Params...))
+{
+	duk_push_c_function(ctx, (realBindFunc <ReturnType, Params... > ), DUK_VARARGS );
+	duk_push_pointer( ctx, reinterpret_cast<void *>( func_ptr ) );
+	duk_put_prop_string( ctx, -2, "_func_ptr");
+	duk_put_global_string( ctx, name );
+}
+
 
 int main(int argc, char *argv[]) {
 	duk_context *ctx = duk_create_heap_default();
@@ -36,8 +100,10 @@ int main(int argc, char *argv[]) {
 	duk_put_global_string(ctx, "print");
 	duk_push_c_function(ctx, native_adder, DUK_VARARGS);
 	duk_put_global_string(ctx, "adder");
-	duk_eval_string(ctx, "print('Hello world!');");
 
+	bindFunc( ctx, "hello2", hello2 );
+
+	duk_eval_string(ctx, "print(hello2());");
 	duk_eval_string(ctx, "print('2+3=' + adder(2, 3));");
 	duk_pop(ctx);  /* pop eval result */
 
